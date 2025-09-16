42e646548a88038197ea2f14de4ce478424ac762
Matinpouresmail <matinpouresmail@gmail.com>
2025-09-16 14:15:08 +0330

    14040625

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById('toggle-btn');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  if(body.classList.contains('dark-mode')) {
+    toggleBtn.textContent = '☀️ Light Mode';
+  } else {
+    toggleBtn.textContent = '🌙 Dark Mode';
+  }
+});
